c4f306c6d5bca72aec29b5674fae8b84cd11ed6f
Matthew Herron <matthew.herron77@gmail.com>
2017-10-04 21:56:07 -0500

    Etherium script v1.0.0

---
+++ ether.py
@@ -0,0 +1,53 @@
+
+class Mining:
+    def __init__(self):
+        self.eth_usd = 180.0
+        self.eth_cost_per_megahash = 30 / self.eth_usd
+        self.start_megahash = 0
+        self.target_megahash = 150
+        self.daily_eth_per_megahash = 0.00051376
+        self.reinvest_ratio = 0.8
+        self.contract_length = 730
+        self.contracts = []
+        self.paid_off = False
+
+        self.megahashpower = self.start_megahash
+        self.usd_payout = self.megahashpower * self.daily_eth_per_megahash * self.eth_usd
+        self.ether_balance = self.eth_cost_per_megahash * self.target_megahash
+        self.reinvest_ether_balance = self.ether_balance
+        self.pocket_ether_balance = 0
+
+    def start(self):
+        for day in range(0,1460):
+            if self.reinvest_ether_balance >= (self.eth_cost_per_megahash * self.target_megahash):
+                self.buy_megahash(day)
+            self.payout(day)
+            self.expire(day)
+            print 'DAY', day
+            print 'start balance (USD):', self.ether_balance * self.eth_usd
+            print '# contracts:', len(self.contracts)
+            print 'reinvest balance (USD):', self.reinvest_ether_balance * self.eth_usd
+            print 'pocket balance (USD):', self.pocket_ether_balance * self.eth_usd
+            print 'megahash power (MH/s):', self.megahashpower
+
+    def buy_megahash(self, day):
+        self.contracts.append(day + self.contract_length)
+        self.megahashpower += self.target_megahash
+        self.reinvest_ether_balance -= (self.eth_cost_per_megahash * self.target_megahash)
+
+    def payout(self, day):
+        payout = self.megahashpower * self.daily_eth_per_megahash
+        self.reinvest_ether_balance += (self.reinvest_ratio * payout)
+        self.pocket_ether_balance += (payout * (1 - self.reinvest_ratio))
+        if (self.pocket_ether_balance >= self.ether_balance) and not self.paid_off:
+            print 'Recouped initial investment on day:', day
+            self.paid_off = True
+        return payout
+
+    def expire(self, day):
+        if len(self.contracts) > 0 and self.contracts[0] == day:
+            self.contracts = self.contracts[1:]
+            self.megahashpower -= self.target_megahash
+
+
+Mining().start()
