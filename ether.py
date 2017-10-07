dbec8b3884bc61e9ff44d40f76121c8b2fb1abdd
Matthew Herron <matthew.herron77@gmail.com>
2017-10-07 11:42:29 -0500

    Fixed renewal simulator

--- ether.py
+++ ether.py
@@ -1,43 +1,59 @@
+import prices
 
 class Mining:
     def __init__(
         self,
-        invest_days=1460,
-        eth_usd=300.0,
-        usd_cost_per_megahash=28,
+        invest_days=3650,
+        eth_usd=400.0,
         start_megahash = 0,
-        target_megahash = 150,
-        daily_eth_per_megahash = 0.00051376,
-        reinvest_ratio = 0.5,
+        initial_investment = 15000,
+        daily_eth_per_megahash = 0.000141,
+        reinvest_ratio = 0.7,
         contract_length = 730,
+        full_reinvest_til_even = False,
         renew = True):
 
+        self.plans = prices.plans()
         self.invest_days = invest_days
         self.eth_usd = eth_usd
-        self.usd_cost_per_megahash = usd_cost_per_megahash
         self.start_megahash = start_megahash
-        self.target_megahash = target_megahash
+        self.target_megahash = self.lookupHash(initial_investment)['hashrate']
         self.daily_eth_per_megahash = daily_eth_per_megahash
         self.reinvest_ratio = reinvest_ratio
+        self.full_reinvest_til_even = full_reinvest_til_even
         self.contract_length = contract_length
         self.renew = renew
 
         self.contracts = []
         self.paid_off = False
+        self.first_buy = True
         self.payoff_days = self.invest_days
-        self.eth_cost_per_megahash =  float(self.usd_cost_per_megahash) / float(self.eth_usd)
         self.megahashpower = self.start_megahash
         self.usd_payout = self.megahashpower * self.daily_eth_per_megahash * self.eth_usd
-        self.ether_balance = self.eth_cost_per_megahash * self.target_megahash
+        self.ether_balance = self.lookupHash(initial_investment)['cost'] / self.eth_usd
         self.reinvest_ether_balance = self.ether_balance
         self.pocket_ether_balance = 0
 
-    def start(self):
+    def start(self, printout):
         for day in range(0,self.invest_days):
-            if self.reinvest_ether_balance >= (self.eth_cost_per_megahash * self.target_megahash):
-                self.buy_megahash(day)
+            reinvest_plan = self.lookupHash(self.reinvest_ether_balance * self.eth_usd)
+            if self.full_reinvest_til_even:
+                if self.paid_off or self.first_buy:
+                    self.buy_megahash(day, reinvest_plan)
+                    if self.first_buy:
+                        self.first_buy = False
+            else:
+                self.buy_megahash(day, reinvest_plan)
             self.payout(day)
             self.expire(day)
+            if printout:
+                print '-------------------------------------------------------------'
+                print 'DAY', day
+                print 'reinvest plan', reinvest_plan['hashrate'], 'MH'
+                print 'pocket balance:', self.pocket_ether_balance * self.eth_usd
+                print 'reinvest balance:', self.reinvest_ether_balance * self.eth_usd
+                print 'number of contracts:', len(self.contracts)
+                print 'hashpower (MH):', self.megahashpower
 
         return {
             'start_balance_USD': self.ether_balance * self.eth_usd,
@@ -48,40 +64,69 @@ class Mining:
             'megahash_power_MHs': self.megahashpower 
             }
 
-    def buy_megahash(self, day):
-        self.contracts.append(day + self.contract_length)
-        self.megahashpower += self.target_megahash
-        self.reinvest_ether_balance -= (self.eth_cost_per_megahash * self.target_megahash)
+    def buy_megahash(self, day, plan):
+        if plan['cost'] > 0:
+            self.contracts.append({'expires': day + self.contract_length, 'hashrate': plan['hashrate']})
+            self.megahashpower += plan['hashrate']
+            self.reinvest_ether_balance -= (plan['cost'] / self.eth_usd)
 
     def payout(self, day):
         payout = self.megahashpower * self.daily_eth_per_megahash
-        self.reinvest_ether_balance += (self.reinvest_ratio * payout)
-        self.pocket_ether_balance += (payout * (1 - self.reinvest_ratio))
-        if (self.pocket_ether_balance >= self.ether_balance) and not self.paid_off:
+        if self.full_reinvest_til_even:        
+            if self.paid_off:
+                self.reinvest_ether_balance += (self.reinvest_ratio * payout)
+                self.pocket_ether_balance += (payout * (1 - self.reinvest_ratio))
+            else:
+                self.reinvest_ether_balance += payout
+        else:
+            self.reinvest_ether_balance += (self.reinvest_ratio * payout)
+            self.pocket_ether_balance += (payout * (1 - self.reinvest_ratio))            
+
+        if ((self.pocket_ether_balance >= self.ether_balance) or (self.reinvest_ether_balance > self.ether_balance)) and not self.paid_off:
             self.payoff_days = day
             self.paid_off = True
         return payout
 
     def expire(self, day):
-        if len(self.contracts) > 0 and self.contracts[0] == day:
-            self.contracts = self.contracts[1:]
-            self.megahashpower -= self.target_megahash
-            if self.renew and (self.pocket_ether_balance >= (self.target_megahash * self.eth_cost_per_megahash)):
-                self.renew_contract(day)
+        if len(self.contracts) > 0:
+            oldest_contract = self.contracts[0]
+            if oldest_contract['expires'] == day:
+                self.contracts = self.contracts[1:]
+                self.megahashpower -= oldest_contract['hashrate']
+                if self.renew:
+                    self.renew_contract(day, oldest_contract)
 
-    def renew_contract(self, day):
-        self.pocket_ether_balance -= (self.target_megahash * self.eth_cost_per_megahash)
-        self.contracts.append(day + self.contract_length)
-        self.megahashpower += self.target_megahash
+    def renew_contract(self, day, contract):
+        plan = self.lookupHashCost(contract['hashrate'])
+        if plan['cost'] <= (self.pocket_ether_balance * self.eth_usd):
+            self.pocket_ether_balance -= (plan['cost'] / self.eth_usd)
+            self.contracts.append({'expires': day + self.contract_length, 'hashrate': plan['hashrate']})
+            self.megahashpower += plan['hashrate']
+
+    def lookupHash(self, usd):
+        ret = self.plans[0]
+        for plan in self.plans:
+            if plan['cost'] <= usd:
+                ret = plan
+        return ret
+
+    def lookupHashCost(self, hash):
+        ret = self.plans[0]
+        for plan in self.plans:
+            if plan['hashrate'] <= hash:
+                ret = plan
+        return ret
 
 
 # optimze the best reinvest ratio
-def optimize_reinvest_ratio(investment_period_days, eth_avg_price, target_hash, daily_payout_eth, contract_length_days):
+def optimize_reinvest_ratio(investment_period_days, eth_avg_price, initial_investment, daily_payout_eth, contract_length_days):
     print 'Please wait while we optimize for best reinvestment ratio... '
     max_pocket = 0
-    for i in range(0, 1000):    
+    best_ratio = 0
+    best_payoff = investment_period_days
+    for i in range(0, 1000):
         ratio = float(i) / 1000
-        mining = Mining(invest_days=investment_period_days, eth_usd=eth_avg_price, target_megahash=target_hash, daily_eth_per_megahash=daily_payout_eth, reinvest_ratio=ratio, contract_length=contract_length_days).start()
+        mining = Mining(invest_days=investment_period_days, eth_usd=eth_avg_price, initial_investment=initial_investment, daily_eth_per_megahash=daily_payout_eth, reinvest_ratio=ratio, contract_length=contract_length_days).start(False)
         pocket = mining['pocket_balance_USD']
         if pocket > max_pocket:
             max_pocket = pocket
@@ -89,16 +134,18 @@ def optimize_reinvest_ratio(investment_period_days, eth_avg_price, target_hash, 
             best_payoff = mining['payoff_days']
     return {'best_ratio': best_ratio, 'pocket_balance': max_pocket, 'payoff_days': best_payoff}
 
-investment_period_years = raw_input('How many years do you want to invest? (1-20): ')
-investment_period_days = int(investment_period_years) * 365
-eth_avg_price = raw_input('What do you think the avg price of ETH will be over that time? ($): ')
-eth_avg_price = float(eth_avg_price)
-target = raw_input('How much do you want to invest up front? ($): ')
-target_hash = float(target) / 28
-daily_payout_eth = raw_input('How much ETH is paid out daily on avg by the miner? (ETH): ')
-daily_payout_eth = float(daily_payout_eth)
-contract_length_years = raw_input('What is the contract length in years? (1-5): ')
-contract_length_days = int(contract_length_years) * 365
+def prompt():
+    investment_period_years = raw_input('How many years do you want to invest? (1-20): ')
+    investment_period_days = int(investment_period_years) * 365
+    eth_avg_price = raw_input('What do you think the avg price of ETH will be over that time? ($): ')
+    eth_avg_price = float(eth_avg_price)
+    initial_investment = raw_input('How much do you want to invest up front? ($): ')
+    daily_payout_eth = raw_input('How much ETH is paid out daily on avg by the miner? (ETH): ')
+    daily_payout_eth = float(daily_payout_eth)
+    contract_length_years = raw_input('What is the contract length in years? (1-5): ')
+    contract_length_days = int(contract_length_years) * 365
+    print optimize_reinvest_ratio(investment_period_days, eth_avg_price, initial_investment, daily_payout_eth, contract_length_days)
 
-print optimize_reinvest_ratio(investment_period_days, eth_avg_price, target_hash, daily_payout_eth, contract_length_days)
+#print Mining().start(printout=True)
+prompt()
     
---
+++ prices.py
@@ -0,0 +1,95 @@
+def plans():
+    return [
+        {
+            'hashrate': 0,
+            'cost': 0
+        },
+        {
+            'hashrate': 1.0,
+            'cost': 30
+        },
+        {   
+            'hashrate': 2.0,
+            'cost': 60
+        },
+        {
+            'hashrate': 3.0,
+            'cost': 90
+        },
+        {
+            'hashrate': 4.0,
+            'cost': 120
+        },
+        {
+            'hashrate': 5.0,
+            'cost': 150
+        },
+        {
+            'hashrate': 7.5,
+            'cost': 225
+        },
+        {
+            'hashrate': 10.0,
+            'cost': 300
+        },
+        {
+            'hashrate': 15.0,
+            'cost': 450
+        },
+        {
+            'hashrate': 20.0,
+            'cost': 600
+        },
+        {
+            'hashrate': 25.0,
+            'cost': 750
+        },
+        {
+            'hashrate': 30.0,
+            'cost': 869
+        },
+        {
+            'hashrate': 40.0,
+            'cost': 1160
+        },
+        {
+            'hashrate': 50.0,
+            'cost': 1450
+        },
+        {
+            'hashrate': 55.0,
+            'cost': 1595
+        },
+        {
+            'hashrate': 70.0,
+            'cost': 2030
+        },
+        {
+            'hashrate': 80.0,
+            'cost': 2320
+        },
+        {
+            'hashrate': 90.0,
+            'cost': 2610
+        },
+        {
+            'hashrate': 100.0,
+            'cost': 2800
+        },
+        {
+            'hashrate': 175.0,
+            'cost': 4900
+        },
+        {
+            'hashrate': 250.0,
+            'cost': 7000
+        },
+        {
+            'hashrate': 500.0,
+            'cost': 14000
+        },
+        {
+            'hashrate': 1000.0,
+            'cost': 28000
+        }
+    ]
